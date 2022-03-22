f126e7c01369fd584785c813977c2cb658d29b63
delattre1 <daniieeldelattre@gmail.com>
2022-03-21 22:47:35 -0300

    merged api

--- api.py
+++ api.py
@@ -27,6 +27,10 @@ class Item_name(BaseModel):
 class Cart(BaseModel):
     cart: dict(Product)
 
+app = FastAPI()
+
+
+CARRINHOS = {}
 INVENTORY = {
                 'nescau'  : {'qtd': 6, 'preco': 3, 'descricao': 'blabla'} ,
                 'repolho' : {'qtd': 2, 'preco': 22, 'descricao': 'blabla'},
@@ -35,16 +39,89 @@ INVENTORY = {
 CART = {
         '123': {
                 'nescau'  : {'qtd': 6, 'preco': 3, 'descricao': 'blabla'},
-                'awp'     : {'qtd': 6, 'preco': 3, 'descricao': 'que ota?'}
+                'awp'  : {'qtd': 6, 'preco': 3, 'descricao': 'que ota?'}
                },
 
         '456': {
                 'banana'  : {'qtd': 2, 'preco': 23, 'descricao': 'blabla'},
-                'glock'   : {'qtd': 5, 'preco': 15, 'descricao': 'que ota?'}
+                'glock'  : {'qtd': 5, 'preco': 15, 'descricao': 'que ota?'}
                }
        }
+
+
+#Routes to interact with products cart
+@app.post('/cart/{user_id}')
+async def create_cart(user_id):
+    CART[user_id] = {}
+    return {'message': f'Carrinho user_id: {user_id} criado com sucesso!', 
+            'carrinho': CART[user_id]}
+
+@app.put('/cart/{user_id}')
+async def update_cart(user_id, cart: Cart):
+    if user_id not in CART:
+        return {'message': 'Usuario nao tem carrinho!'}
+
+    CART[user_id] = cart
+    return {'message': f'Carrinho user_id: {user_id} atualizado com sucesso!', 
+            'carrinho': CART[user_id]}
+
+@app.delete('/cart/{user_id}')
+async def delete_cart(user_id):
+    if user_id not in CART:
+        return {'message': 'Usuario nao tem carrinho!'}
+    del CART[user_id]
+    return {'message': f'Carrinho user_id: {user_id} deletado com sucesso!'}
+
+@app.get('/cart/{user_id}')
+async def get_products_from_cart(user_id):
+    if user_id not in CART:
+        return {'message': 'Usuario nao tem carrinho!'}
+    return {'carrinho': CART[user_id]}
+
 
 #Ping route to check if server is up and running
 @app.get('/')
 async def root():
     return {'message': 'Hello from your carrinho de compras'}
+
+
+#Routes to interact with the inventory
+@app.get('/inventory/')
+async def get_all_items_from_inventory():
+    return {'inventory': INVENTORY}
+
+@app.post('/inventory/')
+async def create_item_in_inventory(product: Product):
+    p_name = product.name
+    p_info = product.info
+    INVENTORY[p_name] = p_info
+    return {'inventory': INVENTORY}
+
+@app.put('/inventory/')
+async def update_item_in_inventory(product: Product):
+    p_name = product.name
+    p_info = product.info
+
+    if p_name not in INVENTORY:
+        return {'message': f'product: {p_name} not in inventory!', 'inventory': INVENTORY}
+
+    INVENTORY[p_name] = p_info
+
+    return {'message': f'product: {p_name} updated successfully!', 'inventory': INVENTORY}
+
+@app.delete('/inventory/')
+async def delete_item_from_inventory(item: Item_name):
+    item_name = item.name
+    del INVENTORY[item_name]
+    return {'inventory': INVENTORY}
+
+
+
+    
+
+
+
+
+
+
+
